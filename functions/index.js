d8fd415f506cd471fe15c66bfba5cadc692b2155
Yu-sooho <souho456@naver.com>
2023-03-07 22:50:49 +0900

    Feat: 펑션 기본 세팅

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    // Grab the text parameter.
+    const original = req?.query?.text1;
+    const original2 = req?.query?.text2;
+    console.log(original)
+    res.json(original+original2)
+    // Push the new message into Firestore using the Firebase Admin SDK.
+    // const writeResult = await admin.firestore().collection('messages').add({original: original});
+    // Send back a message that we've successfully written the message
+    // res.json({result: `Message with ID: ${writeResult.id} added.`});
+  });
+  
